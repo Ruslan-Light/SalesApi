c0d9181f194e72f8ba54f2dc66bd212b8b25ec5b
Ruslan-Light <73093065+Ruslan-Light@users.noreply.github.com>
2022-10-16 23:29:37 +0300

    New Startup

---
+++ SalesApi/Presentation/Startup.cs
@@ -0,0 +1,88 @@
+using Application;
+using Application.Extensions;
+using Infrastructure;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
+using Presentation.Configuration;
+using System;
+using System.IO;
+
+namespace Presentation
+{
+    public class Startup
+    {
+        public Startup(IWebHostEnvironment env)
+        {
+            Environment = env;
+
+            Configuration = new ConfigurationBuilder()
+                .AddJsonFile("appsettings.json")
+                .AddJsonFile($"appsettings.{Environment.EnvironmentName}.json")
+                .Build();
+        }
+
+        public IConfiguration Configuration { get; }
+        IWebHostEnvironment Environment { get; }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddInfrastructureDI(Configuration);
+            services.AddApplicationDI(Configuration);
+
+            services.AddCors();
+
+            services.AddControllers();
+
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo
+                {
+                    Title = "Sales Service API",
+                    Version = "v1",
+                    Description = "API для покупок"
+                });
+
+                c.UseAllOfToExtendReferenceSchemas();
+
+                c.IncludeXmlComments(Path.Combine(AppContext.BaseDirectory, "Presentation.xml"));
+            });
+        }
+
+        public void Configure(IApplicationBuilder app,
+            IWebHostEnvironment env,
+            ILoggerFactory loggerFactory)
+        {
+            loggerFactory.AddFile();
+
+            if (env.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI(options =>
+                {
+                    options.SwaggerEndpoint("/swagger/v1/swagger.json", "v1");
+                    options.RoutePrefix = string.Empty;
+                });
+            }
+
+            app.UseCustomExceptionHandler();
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+            app.UseRouting();
+            app.UseCors(builder => builder.AllowAnyOrigin());
+
+            app.UseAuthentication();
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
